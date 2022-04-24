b85a849d59a97ff28223d88e7e3e8922bc3c72fa
Binh Nguyen <labinhan@gmail.com>
2022-04-24 16:08:23 +0200

    final version?

--- src/test/java/com/stager/feeddgsjava/FeedTests.java
+++ src/test/java/com/stager/feeddgsjava/FeedTests.java
@@ -6,7 +6,10 @@ import com.netflix.graphql.dgs.DgsQuery;
 import com.netflix.graphql.dgs.DgsQueryExecutor;
 import com.netflix.graphql.dgs.InputArgument;
 import com.netflix.graphql.dgs.autoconfig.DgsAutoConfiguration;
+import com.netflix.graphql.dgs.client.codegen.GraphQLQueryRequest;
 import com.stager.feeddgsjava.model.Event;
+import com.stager.graphql.generated.client.EventsGraphQLQuery;
+import com.stager.graphql.generated.client.EventsProjectionRoot;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -26,35 +29,110 @@ public class FeedTests {
     @Autowired
     DgsQueryExecutor dgsQueryExecutor;
 
-    @Autowired
-    GraphQLTestTemplate graphQLTestTemplate;
-
-    @Autowired
-    TestRestTemplate testRestTemplate;
-
     @Test
-    void EventsId(){
+    void EventsId() {
         List<Integer> events = dgsQueryExecutor.executeAndExtractJsonPath(
                 " { events { id }}",
                 "data.events[*].id");
 
-        //find the new event that was created through the monolith
-        assertThat(events).containsSequence(1,1,1,2);
+        assertThat(events).containsSequence(1, 1, 1, 2);
+    }
+
+    @Test
+    public void eventsWithQueryApi() {
+        GraphQLQueryRequest graphQLQueryRequest = new GraphQLQueryRequest(
+                new EventsGraphQLQuery.Builder().build(),
+                new EventsProjectionRoot().id()
+        );
+        List<Integer> ids = dgsQueryExecutor.executeAndExtractJsonPath(graphQLQueryRequest.serialize(), "data.events[*].id");
+        assertThat(ids).containsSequence(1, 1, 1, 1, 2);
+    }
+
+    @Test
+    void EventPrimaryGroupId() {
+        List<Integer> events = dgsQueryExecutor.executeAndExtractJsonPath(
+                " { events { primaryEventGroup_id }}",
+                "data.events[*].primaryEventGroup_id");
+
+        assertThat(events).containsSequence(1, 1, 1, 2);
+    }
+
+    @Test
+    void EventOrganizerId() {
+        List<Integer> events = dgsQueryExecutor.executeAndExtractJsonPath(
+                " { events { organizer_id }}",
+                "data.events[*].organizer_id");
+
+        assertThat(events).containsSequence(1, 1, 1, 1);
+    }
+
+    @Test
+    void EventVenueId() {
+        List<Integer> events = dgsQueryExecutor.executeAndExtractJsonPath(
+                " { events { venue_id }}",
+                "data.events[*].venue_id");
+
+        assertThat(events).containsSequence(1, 1, 1, 1);
+    }
+
+    @Test
+    void EventSeriesId() {
+        List<Integer> events = dgsQueryExecutor.executeAndExtractJsonPath(
+                " { events { series_id }}",
+                "data.events[*].series_id");
+
+        assertThat(events).containsSequence(1, 1, 1, 2);
+    }
+
+    @Test
+    void VenueId() {
+        List<Integer> events = dgsQueryExecutor.executeAndExtractJsonPath(
+                " { venues { id }}",
+                "data.venues[*].id");
+
+        assertThat(events).containsSequence(1);
+    }
+
+    @Test
+    void VenueOrganizerId() {
+        List<Integer> events = dgsQueryExecutor.executeAndExtractJsonPath(
+                " { venues { organizer_id }}",
+                "data.venues[*].organizer_id");
+
+        assertThat(events).containsSequence(1);
+    }
+
+    @Test
+    void OrganizerId() {
+        List<Integer> events = dgsQueryExecutor.executeAndExtractJsonPath(
+                " { organizers { id }}",
+                "data.organizers[*].id");
+
+        assertThat(events).containsSequence(1);
+    }
+
+    @Test
+    void EventGroupId() {
+        List<Integer> events = dgsQueryExecutor.executeAndExtractJsonPath(
+                " { eventgroups { id }}",
+                "data.eventgroups[*].id");
+
+        assertThat(events).containsSequence(1, 1, 1, 1, 2);
+    }
+
+    @Test
+    void EventGroupOrganizerId() {
+        List<Integer> events = dgsQueryExecutor.executeAndExtractJsonPath(
+                " { eventgroups { organizer_id }}",
+                "data.eventgroups[*].organizer_id");
+
+        assertThat(events).containsSequence(1, 1, 1, 1, 1);
     }
 
 
 
-
-
-//    @Test
-//    void TestEvents(){
-//        List<String> titles = dgsQueryExecutor.executeAndExtractJsonPath(
-//                "{ eventTestList { id primaryEventGroup_id organizer_id venue_id series_id title}}",
-//                "data.eventTestList[*].title"
-//
-//        );
-//
-//        assertThat(titles).contains("event1");
-//
-//    }
 }
+
+
+
+
--- src/test/java/com/stager/feeddgsjava/ResolverTests.java
+++ src/test/java/com/stager/feeddgsjava/ResolverTests.java
@@ -13,11 +13,11 @@ import java.io.IOException;
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.core.IsEqual.equalTo;
 import static org.skyscreamer.jsonassert.JSONAssert.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT, classes = TestApplication.class)
 public class ResolverTests {
-    
-    //Also known as integration tests
+
     @Autowired
     GraphQLTestTemplate graphQLTestTemplate;
 
@@ -25,6 +25,7 @@ public class ResolverTests {
     public void shouldAbleToGetEventData() throws IOException, JSONException {
         GraphQLResponse graphQLResponse = graphQLTestTemplate.postForResource("request/eventquery.graphqls");
         assertThat(graphQLResponse.isOk(),equalTo(true));
+        assertNotNull(graphQLResponse);
         assertEquals(FileReaderHelper.read("response/eventquery.json"), graphQLResponse.getRawResponse().getBody(), true);
     }
 
@@ -32,6 +33,7 @@ public class ResolverTests {
     public void shouldAbleToGetEventGroupData() throws IOException, JSONException {
         GraphQLResponse graphQLResponse = graphQLTestTemplate.postForResource("request/eventgroupquery.graphqls");
         assertThat(graphQLResponse.isOk(),equalTo(true));
+        assertNotNull(graphQLResponse);
         assertEquals(FileReaderHelper.read("response/eventgroupquery.json"), graphQLResponse.getRawResponse().getBody(), true);
     }
 
@@ -39,6 +41,7 @@ public class ResolverTests {
     public void shouldAbleToGetOrganizersData() throws IOException, JSONException {
         GraphQLResponse graphQLResponse = graphQLTestTemplate.postForResource("request/organizersquery.graphqls");
         assertThat(graphQLResponse.isOk(),equalTo(true));
+        assertNotNull(graphQLResponse);
         assertEquals(FileReaderHelper.read("response/organizersquery.json"), graphQLResponse.getRawResponse().getBody(), true);
     }
 
@@ -46,6 +49,7 @@ public class ResolverTests {
     public void shouldAbleToGetVenueData() throws IOException, JSONException {
         GraphQLResponse graphQLResponse = graphQLTestTemplate.postForResource("request/venuequery.graphqls");
         assertThat(graphQLResponse.isOk(),equalTo(true));
+        assertNotNull(graphQLResponse);
         assertEquals(FileReaderHelper.read("response/venuequery.json"), graphQLResponse.getRawResponse().getBody(), true);
     }
 }
